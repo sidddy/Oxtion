81379616c28c040d58775b92233245e799ac3199
sidddy <anon@anon.anon>
2017-12-22 23:24:51 +0100

    Enabled remote debugging, set up initial Nextion connection (updating temperatures, woohoo\¡)

--- esp8266/oxtion/oxtion.ino
+++ esp8266/oxtion/oxtion.ino
@@ -1,6 +1,10 @@
-#include "ESPHelper.h"
+#include <ESPHelper.h>
 #include <ArduinoJson.h>
-#include "Tasker.h"
+#include <Tasker.h>
+#include <RemoteDebug.h>
+
+#include <Nextion.h>
+
 
 #include "config.h"
 
@@ -12,7 +16,7 @@
 #define MAX_FILENAME_LEN 256
 
 
-// State variables, reflecting the current state determined via Octoprint API
+// State variables, reflecting the current state determined via Octoprint API / MQTT
 int c_state = 0;
 float temp_t0_act = -1;
 float temp_t0_tar = -1;
@@ -25,25 +29,36 @@ int job_time_left = -1;
 
 ESPHelper myESP(&homeNet);
 WiFiClient client;
+RemoteDebug Debug;
+
+NexVariable nx_temp_bed_c = NexVariable(2,14,"temp.temp_bed_c");
+NexVariable nx_temp_ext_c = NexVariable(2,14,"temp.temp_ext_c");
+
+NexPage page2    = NexPage(2, 0, "temp");
+
+NexTouch *nex_listen_list[] = 
+{
+    NULL
+};
 
 Tasker octoTasker(false);
 
 // Open connection to the HTTP server
 bool connect(const char* hostName) {
-    //Serial.print("Connect to ");
-    //Serial.println(hostName);
+    Debug.print("Connect to ");
+    Debug.println(hostName);
     
     client.setTimeout(750);
     bool ok = client.connect(hostName, 80);
     
-    //Serial.println(ok ? "Connected" : "Connection Failed!");
+    //Debug.println(ok ? "Connected" : "Connection Failed!");
     return ok;
 }
 
 // Send the HTTP GET request to the server
 bool sendRequest(const char* host, const char* resource) {
-    //Serial.print("GET ");
-    //Serial.println(resource);
+    Debug.print("GET ");
+    Debug.println(resource);
     
     client.print("GET ");
     client.print(resource);
@@ -60,6 +75,14 @@ bool sendRequest(const char* host, const char* resource) {
 
 // Skip HTTP headers so that we are at the beginning of the response's body
 bool skipResponseHeaders() {
+    // Check HTTP status
+    char status[32] = {0};
+    client.readBytesUntil('\r', status, sizeof(status));
+    if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+      Debug.print(F("Unexpected response: "));
+      Debug.println(status);
+      return false;
+    }
     // HTTP headers end with an empty line
     char endOfHeaders[] = "\r\n\r\n";
     
@@ -67,7 +90,7 @@ bool skipResponseHeaders() {
     bool ok = client.find(endOfHeaders);
     
     if (!ok) {
-        Serial.println("No response or invalid response!");
+        Debug.println("No response or invalid response!");
     }
     
     return ok;
@@ -75,27 +98,27 @@ bool skipResponseHeaders() {
 
 void updateConnectionState(int state) {
     if (c_state != state) {
-        Serial.print("Connection state changed. Old: ");
-        Serial.print(c_state);
-        Serial.print(" New: ");
-        Serial.println(state);
+        Debug.print("Connection state changed. Old: ");
+        Debug.print(c_state);
+        Debug.print(" New: ");
+        Debug.println(state);
         c_state = state;
     }
 }
 
 void updateT0Temperatures(float act, float tar) {
     if (abs(act-temp_t0_act)>0.5) {
-        Serial.print("T0 actual temp changed. Old: ");
-        Serial.print(temp_t0_act);
-        Serial.print(" New: ");
-        Serial.println(act);
+        Debug.print("T0 actual temp changed. Old: ");
+        Debug.print(temp_t0_act);
+        Debug.print(" New: ");
+        Debug.println(act);
         temp_t0_act = act;
     }
     if (tar != temp_t0_tar) {
-        Serial.print("T0 target temp changed. Old: ");
-        Serial.print(temp_t0_tar);
-        Serial.print(" New: ");
-        Serial.println(tar);
+        Debug.print("T0 target temp changed. Old: ");
+        Debug.print(temp_t0_tar);
+        Debug.print(" New: ");
+        Debug.println(tar);
         temp_t0_tar = tar;
     }
     return;
@@ -103,37 +126,40 @@ void updateT0Temperatures(float act, float tar) {
 
 void updateBedTemperatures(float act, float tar) {
     if (abs(act-temp_bed_act)>0.5) {
-        Serial.print("Bed actual temp changed. Old: ");
-        Serial.print(temp_bed_act);
-        Serial.print(" New: ");
-        Serial.println(act);
+        Debug.print("Bed actual temp changed. Old: ");
+        Debug.print(temp_bed_act);
+        Debug.print(" New: ");
+        Debug.println(act);
         temp_bed_act = act;
+        
     }
     if (tar != temp_bed_tar) {
-        Serial.print("Bed target temp changed. Old: ");
-        Serial.print(temp_bed_tar);
-        Serial.print(" New: ");
-        Serial.println(tar);
+        Debug.print("Bed target temp changed. Old: ");
+        Debug.print(temp_bed_tar);
+        Debug.print(" New: ");
+        Debug.println(tar);
         temp_bed_tar = tar;
     }
+    nx_temp_bed_c.setValue((uint32)temp_bed_act);
+    nx_temp_ext_c.setValue((uint32)temp_t0_act);
     return;
 }
 
 void updateJobDetails(const char* file, float comp, int time, int time_left) {
     if (strcmp(file, job_file)) {
-        Serial.print("New Job: ");
-        Serial.println(file);
+        Debug.print("New Job: ");
+        Debug.println(file);
         strncpy(job_file, file, MAX_FILENAME_LEN);
     }
     job_completion = comp;
     job_time = time;
     job_time_left = time_left;
-    Serial.print("Job comp: ");
-    Serial.println(comp);
-    Serial.print("Job time: ");
-    Serial.println(time);
-    Serial.print("Job left: ");
-    Serial.println(time_left);
+    Debug.print("Job comp: ");
+    Debug.println(comp);
+    Debug.print("Job time: ");
+    Debug.println(time);
+    Debug.print("Job left: ");
+    Debug.println(time_left);
 }
 
 bool readConnectionContent() {
@@ -145,7 +171,7 @@ bool readConnectionContent() {
     JsonObject& root = jsonBuffer.parseObject(client);
     
     if (!root.success()) {
-        Serial.println("JSON parsing failed!");
+        Debug.println("JSON parsing failed!");
         return false;
     }
     
@@ -171,7 +197,7 @@ bool readPrinterContent() {
     JsonObject& root = jsonBuffer.parseObject(client);
     
     if (!root.success()) {
-        Serial.println("JSON parsing failed!");
+        Debug.println("JSON parsing failed!");
         return false;
     }
     
@@ -190,19 +216,21 @@ bool readJobContent() {
     JsonObject& root = jsonBuffer.parseObject(client);
     
     if (!root.success()) {
-        Serial.println("JSON parsing failed!");
+        Debug.println("JSON parsing failed!");
         return false;
     }
+
     
     
-    updateJobDetails((const char*)root["job"]["file"]["name"],(float)root["progress"]["completion"],(int)root["progress"]["printTime"],(int)root["progress"]["printTimeLeft"]);
+    
+    updateJobDetails((const char*)(root["job"]["file"]["name"] | "N/A"),(float)(root["progress"]["completion"]|0),(int)(root["progress"]["printTime"]|0),(int)(root["progress"]["printTimeLeft"]|0));
     
     
     return true;
 }
 
 void disconnect() {
-    //Serial.println("Disconnect");
+    //Debug.println("Disconnect");
     client.stop();
 }
 
@@ -239,27 +267,39 @@ void getOctoJobState(int) {
 
 void setup() {
     
-    Serial.begin(115200);	//start the serial line
-    delay(500);
+    //Serial.begin(115200);	//start the serial line
+    //delay(500);
+
+    Debug.begin("oxtion");
+    //Debug.setSerialEnabled(true);
+
+    Debug.setResetCmdEnabled(true); // Enable the reset command
     
-    Serial.println("Starting Up, Please Wait...");
+    Debug.println("Starting Up, Please Wait...");
     
     myESP.OTA_enable();
-    myESP.OTA_setPassword("ota.pass");
+    //myESP.OTA_setPassword("ota.pass");
+    myESP.OTA_setHostname("oxtion");
     
     myESP.begin();
+
+    nexInit();
     
     octoTasker.setInterval(getOctoConnectionState, 5000);
     octoTasker.setInterval(getOctoPrinterState, 5000); 
     octoTasker.setInterval(getOctoJobState, 5000); 
+
+    page2.show();
     
-    
-    Serial.println("Initialization Finished.");
+    Debug.println("Initialization Finished.");
 }
 
 void loop(){
     myESP.loop();  //run the loop() method as often as possible - this keeps the network services running
     octoTasker.loop();
+    Debug.handle();
+    nexLoop(nex_listen_list);
+
     
     delay(25);
     
