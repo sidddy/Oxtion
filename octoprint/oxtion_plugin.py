5521a619ad224114ffd663774c4afb17881ad4fb
sidddy <mail@svenhenkel.de>
2018-03-21 09:17:30 +0100

    Plugin: Seperate from octoled again

--- octoprint/oxtion_plugin.py
+++ octoprint/oxtion_plugin.py
@@ -20,42 +20,16 @@ class OxtionPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.EventHandlerP
 
 		self.mqtt_publish("oxtion/misc", "Oxtion plugin startup")
 		self._logger.info("Oxtion Plugin started.")
-		self.mqtt_publish("oxtion/led/mode", "0") # Mode: Startup
-		self.mqtt_publish("octoled/mode", "0") # Mode: Startup
 
 	def on_event(self, event, payload):
-		self.mqtt_publish("oxtion/misc", "event: " + event)
-		if event in ["Connected", "PrintDone"] :
-			self.mqtt_publish("oxtion/led/mode", "1"); # Mode: Standby
-			self.mqtt_publish("octoled/mode", "1"); # Mode: Standby 
-		if event == "Disconnected":
-			self.mqtt_publish("oxtion/led/mode", "4"); # Mode: Disconnected
-			self.mqtt_publish("octoled/mode", "4"); # Mode: Disconnected
 		if event == "PrintStarted":
-			self.mqtt_publish("oxtion/led/mode", "2"); # Mode: Printing
-			self.mqtt_publish("octoled/mode", "2"); # Mode: Printing
 			self._repeat_timer = octoprint.util.RepeatedTimer(15, self.send_progress)
 			self._repeat_timer.start()
 			self._logger.info("Oxtion Plugin progress reporting started.")  
-		if event in ["PrintFailed", "Error"] :
-			self.mqtt_publish("oxtion/led/mode", "3"); # Mode: Error
-			self.mqtt_publish("octoled/mode", "3"); # Mode: Error 
 		if event in ["PrintFailed", "PrintDone"] :
 			if self._repeat_timer != None:
 				self._repeat_timer.cancel()
 				self._repeat_timer = None
-
-	def handle_Z150(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-		if cmd.startswith("Z150 "):
-			self._logger.info("Z150 Detected: " + cmd)
-			self.mqtt_publish("oxtion/led/rgb", cmd[5:])
-			self.mqtt_publish("octoled/rgb", cmd[5:])
-			return None,
-		if cmd.startswith("Z151 "):
-			self._logger.info("Z151 Detected: " + cmd)
-			self.mqtt_publish("oxtion/led/mode", cmd[5:])
-			self.mqtt_publish("octoled/mode", cmd[5:])
-			return None,
 
 	def send_progress(self):
 		self._logger.info("Oxtion Plugin progress reporting triggered.");
@@ -71,17 +45,9 @@ class OxtionPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.EventHandlerP
 		self.mqtt_publish("oxtion/estimate", "{ \"progress\": "+str(currentData["progress"]["completion"])+", \"printtime\": "+str(currentData["progress"]["printTime"])+", \"printtimeleft\": "+str(currentData["progress"]["printTimeLeft"])+" }");
 
 
-##__plugin_implementations__ = [OxtionPlugin()]
-##__plugin_hooks__ = { "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.HandleZ150 }
-
 __plugin_name__ = "Oxtion"
 
 def __plugin_load__():
 	global __plugin_implementation__
 	__plugin_implementation__ = OxtionPlugin()
-
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.handle_Z150
-	}
 
